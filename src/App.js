189c6364ef7bdce3187648292de30f72104b0a3d
StorkKnows <mkneis@yahoo.com>
2019-04-11 17:13:20 -0400

    I change

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-    render() {
-      return (
-        <View style={styles.container}>
-          <Text>
-              You can do it
-          </Text>
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <form>
+          <div className="radio">
+            <label>
+              <input type="radio" value="Plassman Hall" checked={true} />
+              Plassman Hall
+      </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="Swan" />
+              Option 2
+      </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="option3" />
+              Option 3
+      </label>
+          </div>
+        </form>
+      </View>
+    );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#0f0',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
