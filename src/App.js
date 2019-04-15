c52a021887fa4b67f320d2e20809f5170c1aefb5
StorkKnows <mkneis@yahoo.com>
2019-04-15 18:36:40 -0400

    updated style

--- src/App.js
+++ src/App.js
@@ -18,35 +18,25 @@ export default class App extends React.Component {
 
   render() {
 
-    styles={
-        MainPart:{flex:1, },
-        Text_1:{color:"#000000", fontFamily:"lato-regular", fontSize:37, textAlign:"center", top:"10%", },
-        TouchableOpacity_2:{height:50, width:261, backgroundColor:"#20BE1E", top:"70%", alignSelf:"center", borderWidth:1, borderColor:"#000000", },
-        Text_3:{color:"#000000", fontFamily:"lato-regular", fontSize:38, textAlign:"center", },
-        Image_5:{height:"39%", width:"69%", resizeMode:"stretch", top:65, left:"15%", },
-    }
-
-    return  this.state.fontLoaded 
-    ? <View style={styles.MainPart}>
+    if(this.state.fontLoaded) return <View style={styles.MainPart}>
         <Text style={styles.Text_1}>SBU Where?</Text>
         <TouchableOpacity style={styles.TouchableOpacity_2}>
             <Text style={styles.Text_3}>Start!</Text>
         </TouchableOpacity>
         <Image source={[{"uri":"https://keypathedu.com/sites/default/files/styles/banner_media/public/image/2018-08/SBU%20campus.jpg?h=502e75fa&itok=qeAyEgtK"}]} style={styles.Image_5}></Image>
     </View>
-    : null
+    else return null 
 
-}
+  }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#0f0',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  MainPart:{flex:1, },
+  Text_1:{color:"#000000", fontFamily:"lato-regular", fontSize:37, textAlign:"center", top:"10%", },
+  TouchableOpacity_2:{height:50, width:261, backgroundColor:"#20BE1E", top:"70%", alignSelf:"center", borderWidth:1, borderColor:"#000000", },
+  Text_3:{color:"#000000", fontFamily:"lato-regular", fontSize:38, textAlign:"center", },
+  Image_5:{height:"39%", width:"69%", resizeMode:"stretch", top:65, left:"15%", },
+});
 
 /*
 
