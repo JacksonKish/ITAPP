9a8c6367f11085a82f362ee933fe36d46ae2921c
StorkKnows <mkneis@yahoo.com>
2019-04-15 18:19:32 -0400

    Updated start interface with clickable button and some detail for landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Text, TouchableOpacity, Image  } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'lato-regular': require('../assets/Lato-Regular.ttf'),
+    });
+    this.setState({ fontLoaded: true })
+  }
+
+  state = {
+    input: "",
+    fontLoaded: false
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <form>
+
+    styles={
+        MainPart:{flex:1, },
+        Text_1:{color:"#000000", fontFamily:"lato-regular", fontSize:37, textAlign:"center", top:"10%", },
+        TouchableOpacity_2:{height:50, width:261, backgroundColor:"#20BE1E", top:"70%", alignSelf:"center", borderWidth:1, borderColor:"#000000", },
+        Text_3:{color:"#000000", fontFamily:"lato-regular", fontSize:38, textAlign:"center", },
+        Image_5:{height:"39%", width:"69%", resizeMode:"stretch", top:65, left:"15%", },
+    }
+
+    return  this.state.fontLoaded 
+    ? <View style={styles.MainPart}>
+        <Text style={styles.Text_1}>SBU Where?</Text>
+        <TouchableOpacity style={styles.TouchableOpacity_2}>
+            <Text style={styles.Text_3}>Start!</Text>
+        </TouchableOpacity>
+        <Image source={[{"uri":"https://keypathedu.com/sites/default/files/styles/banner_media/public/image/2018-08/SBU%20campus.jpg?h=502e75fa&itok=qeAyEgtK"}]} style={styles.Image_5}></Image>
+    </View>
+    : null
+
+}
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0f0',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+/*
+
+<form>
           <div className="radio">
             <label>
               <input type="radio" value="Plassman Hall" checked={true} />
@@ -14,7 +59,7 @@ export default class App extends React.Component {
           </div>
           <div className="radio">
             <label>
-              <input type="radio" value="Swan" />
+              <input type="radio" value="S" />
               Option 2
       </label>
           </div>
@@ -25,16 +70,5 @@ export default class App extends React.Component {
       </label>
           </div>
         </form>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0f0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+*/
