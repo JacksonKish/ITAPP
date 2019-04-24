61732f64291402ed9faa9c08fe5d94315e7a6f52
JacksonKish <42248025+JacksonKish@users.noreply.github.com>
2019-04-24 14:46:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { StyleSheet, TextInput, View, Text, TouchableOpacity, Image  } from 'rea
 import { Font } from 'expo';
 
 export default class App extends React.Component {
-
   async componentDidMount() {
     await Font.loadAsync({
       'lato-regular': require('../assets/Lato-Regular.ttf'),
     });
+    /*
+    not sure why error here maybe this?
+    */
     this.setState({ fontLoaded: true })
   }
 
   state = {
     input: "",
-    fontLoaded: false
+    fontLoaded: true
   }
 
   render() {
@@ -23,7 +25,9 @@ export default class App extends React.Component {
         <TouchableOpacity style={styles.TouchableOpacity_2}>
             <Text style={styles.Text_3}>Start!</Text>
         </TouchableOpacity>
-        <Image source={[{"uri":"https://keypathedu.com/sites/default/files/styles/banner_media/public/image/2018-08/SBU%20campus.jpg?h=502e75fa&itok=qeAyEgtK"}]} style={styles.Image_5}></Image>
+        <Image source={[{"uri":"https://keypathedu.com/sites/default/files/styles/banner_media/public/image/2018-08/SBU%20campus.jpg?h=502e75fa&itok=qeAyEgtK"}]} 
+        style={styles.Image_5}></Image>
+    <Text style={styles.Text_6}>Created by Michael Kneis Jackson Kish Daniel Nelligan and Barry Gan</Text>
     </View>
     else return null 
 
@@ -31,9 +35,10 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  MainPart:{flex:1, },
+  MainPart:{flex:1, backgroundColor: "#fdb726"},
   Text_1:{color:"#000000", fontFamily:"lato-regular", fontSize:37, textAlign:"center", top:"10%", },
-  TouchableOpacity_2:{height:50, width:261, backgroundColor:"#20BE1E", top:"70%", alignSelf:"center", borderWidth:1, borderColor:"#000000", },
+  TouchableOpacity_2:{height:50, width:261, backgroundColor:"#8B4513", top:"64%", alignSelf:"center", borderWidth:1, borderColor:"#000000", },
   Text_3:{color:"#000000", fontFamily:"lato-regular", fontSize:38, textAlign:"center", },
-  Image_5:{height:"39%", width:"69%", resizeMode:"stretch", top:65, left:"15%", },
+  Image_5:{height:"39%", width:"69%", resizeMode:"stretch", top:65, left:"15%", borderWidth: 1},
+  Text_6:{color:"#000000", fontFamily: "lato-regular", fontSize: 10, top: 300, textAlign: "center"}
 });
