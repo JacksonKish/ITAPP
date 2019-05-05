6673d41406a0a91aa9892d662bc4e2b13a588dae
JacksonKish <42248025+JacksonKish@users.noreply.github.com>
2019-05-05 19:56:49 -0400

    Update QuestionPage.js

--- src/QuestionPage.js
+++ src/QuestionPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, TextInput, View, Text, TouchableOpacity, Image  } from 'react-native';
 import { Font } from 'expo';
+import { DrawerNavigator } from "react-navigation";
+import questionStyles from './qStyleSheet.css'
 
-export default class App extends React.Component {
+class QuestionPage extends React.Component {
 
   async componentDidMount() {
     await Font.loadAsync({
@@ -18,47 +20,55 @@ export default class App extends React.Component {
 
 render() {
 
-    const styles= {
-        MainPart:{flex:1, backgroundColor: "#fdb726"},
-        Text_1:{color:"#000000", fontFamily:"Lato", fontSize:26, textAlign:"center", top:"60%",},
-        Image_2:{height:"50%", width:"86%", borderWidth:2, resizeMode:"stretch", alignSelf:"center", },
-        TouchableOpacity_3:{height:"7%", width:"35%", backgroundColor:"#99531A", position:"absolute", top:"72%", left:"13%", borderWidth : 1 },
-        Text_7:{color: "#FFFFFF", fontFamily:"Lato", fontSize:22, textAlign:"center", top:"25%",},
-        TouchableOpacity_4:{height:"7%", width:"35%", position:"absolute", top:"72%", left:"57%", backgroundColor:"#99531A", borderWidth: 1},
-        Text_8:{color:"#FFFFFF", fontFamily:"Lato", fontSize:22, textAlign:"center", top:"25%", },
-        TouchableOpacity_5:{height:"7%", width:"35%", position:"absolute", top:"85%", left:"57%", backgroundColor:"#99531A", borderWidth:1},
-        Text_10:{color:"#FFFFFF", fontFamily:"Lato", fontSize:22, textAlign:"center", top:"25%", },
-        TouchableOpacity_6:{height:"7%", width:"35%", backgroundColor:"#99531A", position:"absolute", top:"85%", left:"13%", borderWidth: 1},
-        Text_9:{color:"#FFFFFF", fontFamily:"Lato", fontSize:22, textAlign:"center", top:"25%", },
-    }
-
-    const AppNavigator = createStackNavigator({
-      Question: {
-        Home: App,
-        Question: QuestionPage
-      }
-    });
-    
-    export default createAppContainer(AppNavigator);
-
-    return    <View style={styles.MainPart}>
-        <Text style={styles.Text_1}>What building is this?</Text>
-        <Image source={[{"uri":"https://s3-us-west-2.amazonaws.com/asset.plexuss.com/college/overview_images/2716_st_bonaventure_university_01.jpg"}]} style={styles.Image_2}></Image>
-        <TouchableOpacity style={styles.TouchableOpacity_3}>
-            <Text style={styles.Text_7}>Option</Text>
+        return    <View style={questionStyles.MainPart}>
+        
+        <Text style={questionStyles.Text_1}>What building is this?</Text>
+        
+        <Image source={require ("./Campus Pics/Butler (3).JPG")} style={questionStyles.Image_2}></Image>
+        <TouchableOpacity style={questionStyles.TouchableOpacity_3}>
+            <Text style={questionStyles.Text_7}>Option</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.TouchableOpacity_4}>
-            <Text style={styles.Text_8}>Option</Text>
+        
+        <TouchableOpacity style={questionStyles.TouchableOpacity_4}>
+            <Text style={questionStyles.Text_8}>Option</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.TouchableOpacity_5}
+        
+        <TouchableOpacity style={questionStyles.TouchableOpacity_5}
             onPress = {() => this.props.navigate("QuestionPage")}>
-            <Text style={styles.Text_10}>Option</Text>
+            <Text style={questionStyles.Text_10}>Option</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.TouchableOpacity_6}>
-            <Text style={styles.Text_9}>Option</Text>
+        
+        <TouchableOpacity style={questionStyles.TouchableOpacity_6}>
+            <Text style={questionStyles.Text_9}>Option</Text>
         </TouchableOpacity>
+    
     </View>
 
 }
 
 }
+class genQuestions extends React.Component{
+
+ selector = Math.floor(Math.random)
+
+if (selector > 0 ){
+  
+}
+
+
+pics = [
+      {picture: "./CampusPics/Butler.jpg", answer: "Butler"},
+      {picture: "./CampusPics/Devereux.jpg", answer: "Devereux"},
+      {picture: "./CampusPics/Doyle.jpg", answer: "Doyle"},
+      {picture: "./CampusPics/Friedsam.jpg", answer: "Friedsam"},
+      {picture: "./CampusPics/Hall of Fame.jpg", answer: "Hall of Fame"},
+      {picture: "./CampusPics/Quick.jpg", answer: "Quick"},
+      {picture: "./CampusPics/Walsh.jpg", answer: "Walsh"},
+      {picture: "./CampusPics/Richter.jpg", answer: "Richter"}
+]
+
+
+  
+}
+
+
